ff37ac5adaa1d953a11903b6ae222277952b4036
Johann M <jmedinap@gmail.com>
2021-10-31 16:03:59 -0500

    adds store implementation

---
+++ MyCouchCloudantStore.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyCouch.CloudantIAM
+{
+    public class MyCouchCloudantStore : MyCouchStore
+    {
+        public MyCouchCloudantStore(string serverAddress, string dbName = null) : this(new MyCouchCloudant(serverAddress, dbName)) { }
+
+        public MyCouchCloudantStore(Uri serverAddress, string dbName = null) : this(new MyCouchCloudant(serverAddress, dbName)) { }
+
+        public MyCouchCloudantStore(IMyCouchClient client) : base(client) {}
+    }
+}
